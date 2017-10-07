2ce6e9cf7c590f13bb843a5a1ee7691f50736fbd
jrising2 <jrising2@kent.edu>
2017-10-07 11:55:49 -0400

    Added rest of project

---
+++ app/src/main/java/com/example/josh/jargon/AudioHandler.java
@@ -0,0 +1,30 @@
+package com.example.josh.jargon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.ArrayList;
+import android.util.Log;
+
+
+/**
+ * Class responsible for taking in audio input and returning strings
+ * To be used for outputting to the user and selecting of jargon to be
+ * actively worked on.
+ */
+
+public class AudioHandler{
+    static int listenIntervals = 15; // seconds
+    static SpeechRecognizer speechAPI;
+
+    public AudioHandler(){
+//        speechAPI = SpeechRecognizer.createSpeechRecognizer();
+    }
+}
